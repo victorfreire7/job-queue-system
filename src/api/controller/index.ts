ae6e05cc28a4b698fc6d1c4d77f5653cf3dbb01a
victorfreire7 <vhugo.freire159@gmail.com>
2026-04-24 19:39:43 -0300

    add handle conditional

--- src/api/controller/index.ts
+++ src/api/controller/index.ts
@@ -2,7 +2,6 @@ import redis from '../config/redis.ts';
 import { faker } from '@faker-js/faker';
 // import User from '../model/User.ts';
 
-
         // 1. criar set: count + "email pass"
         // 2. verificar qual handle esta mais disponivel
         // 3. adicionar um append no value com o handle que esta mais vazio
@@ -14,15 +13,22 @@ const store = async (req: any, res: any) => {
         const password = faker.internet.password();
         const info:any = `${email} ${password}`;
 
-        let handle:string | any = 'HANDLE_2';
+        const HANDLE_1 = await redis.lRange("HANDLE_1", 0, -1) // RETORNO = ARRAY
+        const HANDLE_2 = await redis.lRange("HANDLE_2", 0, -1) // RETORNO = ARRAY
+        
+        let handle:string | any = 'HANDLE_1';
+        if(HANDLE_1.length > HANDLE_2.length){            
+            handle = 'HANDLE_2';
+        }
+
+        // LPUSH (HANDLE_1 || HANDLE_2) "EMAIL PASSWORD"
 
         await redis.lPush(
             handle, 
             info 
         );
 
-        const HANDLE_1 = await redis.lRange("HANDLE_1", 0, -1) // RETORNO = ARRAY
-        const HANDLE_2 = await redis.lRange("HANDLE_2", 0, -1) // RETORNO = ARRAY
+
 
         console.log(HANDLE_1)
         console.log(HANDLE_2)
