a2df3376228d294e4d877385188308d64fbbaebe
victorfreire7 <vhugo.freire159@gmail.com>
2026-04-25 00:58:22 -0300

    remove redis & mongo teste connection

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,16 +1,6 @@
 import express from 'express';
-import redis from './config/redis.ts';
-import mongodb from '../handler/config/mongo.ts'; // CONEXAO SENDO FEITA EM SERVER.TS TEMPORARIAMENTE. SERA REMOVIDO POSTERIORMENTE QUANDO OS HANDLERS FOREM CRIADOS.
 import controller from './controller/index.ts'
 const app = express();
-
-await mongodb()
-.then(() => {
-    app.emit('mongodb OK')
-});
-
-await redis.connect()
-.then(()=>console.log('redis connect'));
 
 app.post('/', controller.store);
 
