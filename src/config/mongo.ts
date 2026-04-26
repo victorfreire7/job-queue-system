13acdc2b87aa0006dbe9112bb5c715bf63bd432c
victorfreire7 <vhugo.freire159@gmail.com>
2026-04-26 19:33:42 -0300

    fix dotenv config path

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -1,24 +1,15 @@
 import mongoose from "mongoose";
 import { config } from "dotenv";
-config({ path: '../../.env' });
+config();
 
 const connectionstring:any = process.env.MONGO_CONNECTIONSTRING;
 
 const db = async () => {
   try {
     await mongoose.connect(connectionstring);
-    console.log("mongoDB connection OK");
   } catch (error) {
     console.error(`ERRO: ${error}`);
   }
 };
 
 export default db;
-
-// TO CONNECT:
-
-// import mongodb from '../config/mongo.ts'; // CONEXAO SENDO FEITA EM SERVER.TS TEMPORARIAMENTE. SERA REMOVIDO POSTERIORMENTE QUANDO OS HANDLERS FOREM CRIADOS.
-// await mongodb()
-// .then(() => {
-//     app.emit('mongodb OK')
-// });
