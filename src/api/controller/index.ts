752a8d108b592e603a5c4004fda56be967073c5f
victorfreire7 <vhugo.freire159@gmail.com>
2026-04-26 23:25:37 -0300

    increment while loop for seed

--- src/api/controller/index.ts
+++ src/api/controller/index.ts
@@ -1,36 +1,40 @@
 import redis from '../../config/redis.ts';
 import { faker } from '@faker-js/faker';
+let i = 0
+
 
 const store = async (req: any, res: any) => {
-    try {
-        const email = faker.internet.email();
-        const password = faker.internet.password();
-        const info:any = `${email} ${password}`;
-
-        const HANDLE_1 = await redis.lRange("HANDLE_1", 0, -1) // RETORNO = ARRAY
-        const HANDLE_2 = await redis.lRange("HANDLE_2", 0, -1) // RETORNO = ARRAY
-
-        let handle:string | any = 'HANDLE_1';
-        if(HANDLE_1.length > HANDLE_2.length){            
-            handle = 'HANDLE_2';
+    while (i < 1000) {        
+        try {
+            const email = faker.internet.email();
+            const password = faker.internet.password();
+            const info:any = `${email} ${password}`;
+    
+            const HANDLE_1 = await redis.lRange("HANDLE_1", 0, -1) // RETORNO = ARRAY
+            const HANDLE_2 = await redis.lRange("HANDLE_2", 0, -1) // RETORNO = ARRAY
+    
+            let handle:string | any = 'HANDLE_1';
+            if(HANDLE_1.length > HANDLE_2.length){            
+                handle = 'HANDLE_2';
+            }
+    
+            // LPUSH (HANDLE_1 || HANDLE_2) "EMAIL PASSWORD"
+    
+            await redis.lPush(
+                handle, 
+                info 
+            );
+            i++; 
+            continue  
+        } catch (error) {
+            console.error(`ERRO: ${error}`)
         }
-
-        // LPUSH (HANDLE_1 || HANDLE_2) "EMAIL PASSWORD"
-
-        await redis.lPush(
-            handle, 
-            info 
-        );
-
-        res.json({
-            "HANDLE": handle,
-            "EMAIL": email, 
-            "PASSWORD": password
-        })
-
-    } catch (error) {
-        console.error(`ERRO: ${error}`)
     }
+
+    i = 0;
+    res.json({
+        "finish": true,
+    })
 }
 
 export default { store }
