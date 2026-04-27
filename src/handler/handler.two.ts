6204c1788c0c2354308999340e00aee3cfb397a1
victorfreire7 <vhugo.freire159@gmail.com>
2026-04-26 22:50:51 -0300

    finalizing handler opeation

--- src/handler/handler.two.ts
+++ src/handler/handler.two.ts
@@ -1,6 +1,40 @@
-import express from 'express';
-const app = express();
+import mongo_schema from '../api/model/User.ts';
 
-app.listen('8081', () => {
-    console.log(`http://127.0.0.1:8081`)
-})
+import mongodb from '../config/mongo.ts';
+import redis from '../config/redis.ts';
+
+import express from 'express';
+const app: express.Express = express();
+const port: number = 8081;
+
+await redis.connect()
+.then(() => console.log('redis connect'));
+
+await mongodb()
+.then(() => console.log(`mongodb connect in port ${port}`));
+
+app.listen(port, async (): Promise<void> => { 
+    while (true) {   
+        let HANDLE_2: string[] | null = await redis.lRange("HANDLE_2", 0, -1) // RETORNO = ARRAY
+        
+        if(HANDLE_2.length > 0){
+            const user: any = await redis.lmPop( // LRANGE HANDLE_2 0 -1
+                'HANDLE_2',
+                'RIGHT',
+            ); // retorno: [ 'HANDLE_2', [ 'Katrina.Grady29@gmail.com G9KHj1MpR4aaEto' ] ]
+    
+            const user_split: string[] = user[1][0]?.split(' '); // retorno: ['email', 'password']
+            let user_email: any = user_split[0];
+            let user_password: any = user_split[1];
+        
+            console.log(user_split)
+    
+            await mongo_schema.create({ // estou buscando o schema de dentro da API; talvez nao seja a melhor maneira de fazer isso.
+                email: user_email,
+                password: user_password
+            });
+        } else {
+            continue;
+        }
+    }
+});
