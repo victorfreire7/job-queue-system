9a55d0c8364863504f77b751c2d4f78be90d8949
victorfreire7 <vhugo.freire159@gmail.com>
2026-04-27 14:53:25 -0300

    remove redis

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,14 +1,10 @@
 import express from 'express';
-import redis from '../config/redis.ts';
 import controller from './controller/index.ts'
 import mongodb from '../config/mongo.ts';
 const app = express();
 
 await mongodb()
 .then(() => console.log(`mongodb connect in server`));
-
-await redis.connect()
-.then(()=>console.log('redis connect'));
 
 app.post('/', controller.store);
 
