f3562c75e005e990c8c822e9aeed2171e4e51acd
victorfreire7 <vhugo.freire159@gmail.com>
2026-05-01 01:15:45 -0300

    remove any type - improva typing later

--- src/api/controller/index.ts
+++ src/api/controller/index.ts
@@ -1,18 +1,15 @@
+import pkg from 'express';
+const { request, response } = pkg
 import redis from '../../config/redis.ts';
 const handler_numbs: number = 2;
-
-type User = {
-    email: string,
-    password: string
-}
 
 const rand_numb = (min: number, max: number): number => {
     return Math.round(Math.random() * (max - min) + min);
 }
 
-const store = async (req: any, res: any) => {
+const store = async (req: typeof request, res: typeof response) => {
     try {
-        const user: User = { email: req.body.email, password: req.body.password }
+        const user: { email:string,  password:string} = { email: req.body.email, password: req.body.password }
         const rand: number = rand_numb(0.59, handler_numbs + 0.49)
 
         let handle:string = `HANDLE_${rand}`;
